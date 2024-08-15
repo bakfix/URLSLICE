c36b559d10db65a4e3623d73f79c81bbd42ef46f
bakfix <qwertoz322@gmail.com>
2024-08-15 21:08:21 +0700

    make base log and reg view

--- web/views.py
+++ web/views.py
@@ -1,5 +1,55 @@
-from django.shortcuts import render
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import User
+from .serializers import UserSerializer
+import random
+
+SALT = "8b4f6b2cc1868d75ef79e5cfb8779c11b6a374bf0fce05b485581bf4e1e25b96c8c2855015de8449"
+URL = "http://localhost:3000"
 
 
-def profile_view(request):
-    return render(request, 'web/profile.html')
+class RegistrationView(APIView):
+    def post(self, request, format=None):
+        request.data["password"] = make_password(
+            password=request.data["password"], salt=SALT
+        )
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {"success": True, "message": "Вы зарегистрировались на DELTA Short Link!"},
+                status=status.HTTP_200_OK,
+            )
+        else:
+            error_msg = ""
+            for key in serializer.errors:
+                error_msg += serializer.errors[key][0]
+            return Response(
+                {"success": False, "message": error_msg},
+                status=status.HTTP_200_OK,
+            )
+
+
+class LoginView(APIView):
+    def post(self, request, format=None):
+        email = request.data["email"]
+        password = request.data["password"]
+        hashed_password = make_password(password=password, salt=SALT)
+        user = User.objects.get(email=email)
+        if user is None or user.password != hashed_password:
+            return Response(
+                {
+                    "success": False,
+                    "message": "Введен не верный логин или пароль!",
+                },
+                status=status.HTTP_200_OK,
+            )
+        else:
+            return Response(
+                {"success": True, "message": f"Вы вошли в аккаунт {email}!"},
+                status=status.HTTP_200_OK,
+            )
+
+# class CreateShortLinkView():
