fa59ac6da173b24817ead5c6a3a35402779889c3
bakfix <qwertoz322@gmail.com>
2024-08-21 10:41:38 +0700

    add path for admin panel

--- web/urls.py
+++ web/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import RegistrationView, LoginView, CreateShortUrlView, RedirectToLongUrl
+from .views import RegistrationView, LoginView, CreateShortUrlView, RedirectToLongUrl, AdminPanelView
 
 urlpatterns = [
     path("register", RegistrationView.as_view(), name="register"),
     path("login", LoginView.as_view(), name="login"),
     path("short", CreateShortUrlView.as_view(), name="createshorturl"),
     path("<str:short_url>", RedirectToLongUrl.as_view(), name="redirect_to_long_url"),
+    path('admin/users/', AdminPanelView.as_view(), name='admin_users'),
+    path('admin/users/<int:user_id>/', AdminPanelView.as_view(), name='delete_user'),
 ]
