26023f27018f161789b07b7c0fbbfddb80236b6e
bakfix <qwertoz322@gmail.com>
2024-08-22 10:55:04 +0700

    add urls + token for profile

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,17 +1,43 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, redirect } from "react-router-dom";
-import { Avatar } from "flowbite-react";
-import UserIcon from "../images/user.png";
+import axios from "axios";
 import { toast } from "react-toastify";
+import UserIcon from "../images/user.png";
 
+const baseURL = "http://localhost:8000/web/";
 const Profile = (props) => {
   const { isLoggedIn, setIsLoggedIn, name, setName, email, setEmail } = props;
   let navigate = useNavigate();
+  const [urls, setUrls] = useState([]);
+  const [error, setError] = useState('');
+
+
+  useEffect(() => {
+    if (!isLoggedIn) redirect("/");
+
+    const fetchUrls = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        const response = await axios.get('http://localhost:8000/web/user/urls/', {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        });
+        setUrls(response.data);
+      } catch (err) {
+        setError('Ошибка при загрузке данных');
+        console.error(err);
+      }
+    };
+
+    fetchUrls();
+  }, [isLoggedIn]);
 
   const handleLogout = () => {
     setIsLoggedIn(false);
     setName(null);
     setEmail(null);
+    localStorage.removeItem('token');
     navigate("/login");
     toast.success("Вы вышли из аккаунта");
   };
@@ -19,10 +45,6 @@ const Profile = (props) => {
   const handleCreateShortUrl = () => {
     navigate("/shorturl");
   };
-
-  useEffect(() => {
-    if (!isLoggedIn) redirect("/");
-  }, [isLoggedIn]);
 
   return (
     <div className="flex items-center justify-center mt-5">
@@ -43,6 +65,21 @@ const Profile = (props) => {
               Выйти
             </button>
           </div>
+        </div>
+        <div className="mt-6">
+          {error && <p className="text-red-500">{error}</p>}
+          <h3 className="text-lg font-semibold">Мои короткие ссылки:</h3>
+          <ul className="list-disc list-inside mt-2">
+            {urls.length > 0 ? (
+              urls.map((url) => (
+                <li key={url.id}>
+                  Короткая ссылка: <a href={`${baseURL}${url.short_url}`} target="_blank" rel="noopener noreferrer">{`${url.short_url}`}</a>
+                </li>
+              ))
+            ) : (
+              <p>У вас нет коротких ссылок.</p>
+            )}
+          </ul>
         </div>
       </div>
     </div>
