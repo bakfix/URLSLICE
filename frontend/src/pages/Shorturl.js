0cab9111f8818cce093482e411c82a00fe329b32
bakfix <qwertoz322@gmail.com>
2024-08-22 10:56:47 +0700

    add get user token

--- frontend/src/pages/Shorturl.js
+++ frontend/src/pages/Shorturl.js
@@ -4,28 +4,33 @@ import axios from 'axios';
 const URL = process.env.REACT_APP_BACKEND_URL + "/web/short";
 const baseURL = "http://localhost:8000/web/";
 
-
 const Shorturl = () => {
   const [longUrl, setLongUrl] = useState('');
   const [shortUrl, setShortUrl] = useState('');
   const [error, setError] = useState('');
 
-const handleSubmit = async (e) => {
-  e.preventDefault();
-  try {
-    const response = await axios.post(URL, { long_url: longUrl });
-    if (response.status === 201) {
-      setShortUrl(response.data.short_url);
-      setError('');
-    } else {
-      setError('Ошибка создания короткой ссылки');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const token = localStorage.getItem('token');
+      const response = await axios.post(URL, { long_url: longUrl }, {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      console.log(response);
+      if (response.status === 201) {
+        setShortUrl(response.data.short_url);
+        setError('');
+      } else {
+        setError('Ошибка создания короткой ссылки');
+      }
+    } catch (err) {
+      console.error(err);
+      setError('Ошибка сервера: ' + (err.response?.data?.error || 'Неизвестная ошибка'));
     }
-  } catch (err) {
-    setError('Ошибка сервера: ' + (err.response?.data?.error || 'Неизвестная ошибка'));
-  }
-};
-
-
+  };
   return (
     <section className="py-14">
       <div className="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
@@ -50,9 +55,9 @@ const handleSubmit = async (e) => {
             <div className="flex items-center gap-2 mb-2">
               {error && <p className="text-red-500">{error}</p>}
               {shortUrl && (
-             <p>
-                Новая ссылка: <a href={`${baseURL}${shortUrl}`} target="_blank" rel="noopener noreferrer">{`${shortUrl}`}</a>
-              </p>
+                <p>
+                  Новая ссылка: <a href={`${baseURL}${shortUrl}`} target="_blank" rel="noopener noreferrer">{`${shortUrl}`}</a>
+                </p>
               )}
             </div>
             <button
@@ -62,10 +67,8 @@ const handleSubmit = async (e) => {
               Создать короткую ссылку
             </button>
           </form>
-
         </div>
       </div>
-
     </section>
   );
 };
